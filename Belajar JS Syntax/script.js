0abf8fbbf0ce422e78491339fe5d90de39eba3ca
Batiyar <bachtiarahmad6969@gmail.com>
2023-10-05 23:11:30 +0800

    Update JS Syntax

---
+++ Belajar JS Syntax/script.js
@@ -0,0 +1,9 @@
+// File JavaScript (script.js)
+let angka1 = 10;
+let angka2 = "saya belajar JS";
+let angka3 = 55;
+const hasilJumlah = angka1 + angka2;
+const hasilJumlah01 = angka1 + angka3;
+
+document.getElementById("hasil").textContent = hasilJumlah;
+document.getElementById("hasil01").textContent = hasilJumlah01;
