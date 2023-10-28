d7d21e05616d9237185f5bc07f1fd6a992707866
Batiyar <bachtiarahmad6969@gmail.com>
2023-10-28 17:15:02 +0800

    Tugas Step Definision

--- Materi section 7/eduwork-BDD/cypress/e2e/search.js
+++ Materi section 7/eduwork-BDD/cypress/e2e/search.js
@@ -2,15 +2,29 @@ const {Given, When, Then,}= require("@badeball/cypress-cucumber-preprocessor");
 Given ('I am on the homepage', () => {
     cy.visit('http://zero.webappsecurity.com')
 });
+//POSITIVE TEST
 When ('I click the search bar in the upper right corner of the page', () => {
     cy.get("#searchTerm").click()
 });
 When  ('I type the word "online"', () => {
     cy.get("#searchTerm").type('online').clear()
 });
-When ('I press the Enter key', () => {
+When ('I press the Enter button', () => {
     cy.get("#searchTerm").type('online {enter}')
 });
 Then ('I should see results related to the word "online"', () => {
     cy.get('.top_offset').should('contain','online')
+});
+//NEGATIVE TEST
+Given ('I am already on the homepage', () => {
+    cy.visit('http://zero.webappsecurity.com')
+});
+When ('I click the search bar in homepage', () => {
+    cy.get("#searchTerm").click()
+});
+When('I type the random word that not exist in website and enter it', () => {
+    cy.get("#searchTerm").type('saya seorang QA {enter}')
+}); 
+Then('I should see result not found in website', () => {
+    cy.get('.top_offset').should('contain', 'No results were found for the query:')
 });
